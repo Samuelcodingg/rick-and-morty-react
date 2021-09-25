706beff49ba077b05b6d03ffca71d2a587f6a0a4
Samuelcodingg <samuelroman39@gmail.com>
2021-09-24 19:07:01 -0500

    animations added

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -4,7 +4,7 @@ import homeimage from './homeimage.png';
 
 export const HomePage = () => {
     return (
-        <div className="text-center py-5">
+        <div className="text-center py-5 animate__animated animate__fadeIn">
             <h1>Sometimes science is more art than science...</h1>
             <img src={homeimage} alt="rickandmorty" className="home-image" />
             <h4 className="pt-1">Well, that sounded really <span className="fst-italic fw-bolder">cliché</span>, but you know what I mean. Just enjoy this website, old man</h4>
--- src/components/ui/NotFound.js
+++ src/components/ui/NotFound.js
@@ -3,7 +3,7 @@ import rick from './rick-sanchez.png';
 
 export const NotFound = () => {
     return (
-        <div className="text-center p-5 m-4">
+        <div className="text-center p-5 m-4 animate__animated animate__zoomIn">
             <h1>What are you looking for, dude? this page does not exist</h1>
             <img src={rick} alt="rick-sanchez" className="rick"/>
             <h4>Wubba Lubba Dub Dub! look at the top and enter to the other pages man.</h4>
